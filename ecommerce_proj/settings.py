7db2055d6bd0f47dcf476a3e198cd6d5e4e83539
Pranav3303 <160731119+Pranav3303@users.noreply.github.com>
2025-04-17 11:51:59 +0530

    1

--- ecommerce_proj/settings.py
+++ ecommerce_proj/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-27)j15%n$%3ndmu(&d&k0n=97(cmeupu82ts_ahe9hwx=v^-3c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
